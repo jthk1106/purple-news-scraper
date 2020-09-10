783b5effe22aa30e3baf47425c79235660a9d282
Jay <thk116@gmail.com>
2020-09-10 02:46:16 -0700

    took out Authentication from access control allow headers

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(morgan('common'))
 app.use(express.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://purple-news.netlify.app')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 })
 
