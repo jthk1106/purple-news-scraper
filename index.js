70105733f2501100fee00f5d86efb160e05c8d47
Jay <thk116@gmail.com>
2020-09-12 00:19:58 -0700

    commented out cors import and uncommented next call

--- index.js
+++ index.js
@@ -8,25 +8,26 @@ const fox = require('./foxScraper')
 const morgan = require('morgan')
 const cors = require('cors')
 
-const whitelist = ['https://purple-news.netlify.app/', 'http://localhost:3000']
-const corsOptions = {
-  origin: (origin, callback) => {
-    if(whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Origin not on CORS whitelist'))
-    }
-  }
-}
-app.use(cors(corsOptions))
+// const whitelist = ['https://purple-news.netlify.app/', 'http://localhost:3000']
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if(whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Origin not on CORS whitelist'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions))
 
 app.use(morgan('common'))
+
 app.use(express.json())
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-//     next()
-// })
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+})
 
 app.get('/', (req, res) => {
   console.log('whats good world')
