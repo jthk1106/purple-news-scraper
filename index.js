58a041272b6dff3f4a3c17430a42b6978ae21f9f
Jay <thk116@gmail.com>
2020-09-12 01:03:19 -0700

    added express.urlencoded middleware

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const morgan = require('morgan')
 app.use(morgan('common'))
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
